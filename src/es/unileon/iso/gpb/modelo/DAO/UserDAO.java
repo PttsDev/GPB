0429c1d93303cdbcd62dcfeae7f5ee873c43f0ca
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2021-01-05 13:31:52 +0100

    añadido buscar lista de usuarios

--- src/es/unileon/iso/gpb/modelo/DAO/UserDAO.java
+++ src/es/unileon/iso/gpb/modelo/DAO/UserDAO.java
@@ -9,6 +9,9 @@ import es.unileon.iso.gpb.modelo.connection.DBConnection;
 import es.unileon.iso.gpb.modelo.users.User;
 import java.sql.Statement;
 import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 
 /**
  *
@@ -28,12 +31,40 @@ public class UserDAO {
             stat.close();
             connect.close();
         }catch (SQLException e){
-            System.out.println(e.getMessage());
+            System.out.println(e);
             //Llamar a controlador para sacar mensaje por vista TODO
         }
         
     }
     
+    public ArrayList<User> userList(){
+        
+        ArrayList<User> users = new ArrayList<User>();
+        DBConnection connect = new DBConnection();
+        
+        try{
+            PreparedStatement query = connect.getConnection().prepareStatement("SELECT * FROM user");
+            ResultSet rs = query.executeQuery();
+            
+            while( rs.next() ){
+                
+                //String name, String surName, String DNI, String email
+                //ADD USER DATA TODO
+                User user = new User();
+                user.setName(rs.getString("name"));
+                user.setSurName(rs.getString("surname"));
+                user.setDNI(rs.getString("DNI"));
+                user.setEmail(rs.getString("email"));
+                users.add(user);
+            }
+            
+        }catch(Exception e){
+            System.out.println(e);
+            //Llamar al controlador para mensaje por pantalla TODO
+        }
+        
+        return users;
+    }
     //TODO METODOS 
     
 }

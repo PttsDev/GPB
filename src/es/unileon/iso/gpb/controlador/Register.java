b408b558facddf65c654754f05c3ebddf92afab9
agonzg40 <agonzg40@estudiantes.unileon.es>
2021-01-07 20:34:31 +0100

    Puesta la clase Register

---
+++ src/es/unileon/iso/gpb/controlador/Register.java
@@ -0,0 +1,13 @@
+/*
+ * Clase encargada de registrar lo seleccionado por los usuarios
+ */
+package es.unileon.iso.gpb.controlador;
+
+/**
+ *
+ * @author Angel
+ */
+public class Register {
+
+ 
+}

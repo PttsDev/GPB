c0997f4bb94a679fceccf06f03884e0fd9b583d2
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2021-01-08 12:50:08 +0100

    Conectado a la base de datos

--- src/es/unileon/iso/gpb/modelo/connection/DBConnection.java
+++ src/es/unileon/iso/gpb/modelo/connection/DBConnection.java
@@ -17,8 +17,8 @@ public class DBConnection {
     private static final String DB = "db_a6d7c4_gpb";
     private static final String USER = "a6d7c4_gpb";
     private static final String PW = "AlvaroPeroBien1.";
-    private static final String URL = "jdbc:mysql//mysql5044.site4now.net/"+DB;
-    private static final String DRIVER = "com.mysql.jdbc.driver";
+    private static final String URL = "jdbc:mysql://mysql5044.site4now.net/"+DB+"?useSSL=false";
+    private static final String DRIVER = "com.mysql.jdbc.Driver";
     
     private Connection connection = null;
     
@@ -28,7 +28,7 @@ public class DBConnection {
             Class.forName(DRIVER);
             this.connection = DriverManager.getConnection(URL, USER, PW);
             
-            if(connection!=null) System.out.println("Connection to"+DB+"OK");
+            if(connection!=null) System.out.println("Connection to "+DB+" OK");
             
         }catch(Exception e){
             System.out.println(e);

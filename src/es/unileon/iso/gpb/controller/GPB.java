edd421b6aa0b4dc30020414aa4806d84c6887007
Roberto Viejo <47633569+Ptts2@users.noreply.github.com>
2021-01-14 19:41:24 +0100

    Update GPB.java

--- src/es/unileon/iso/gpb/controller/GPB.java
+++ src/es/unileon/iso/gpb/controller/GPB.java
@@ -15,7 +15,7 @@ public class GPB {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        // TODO code application logic here
+        // TODO code application logic here 
         new LogIn().setVisible(true);
     }
 }

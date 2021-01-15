c2cd5ce9df92f820b9058b414bb59556c79b9a08
dcastr03 <47633781+dcastr03@users.noreply.github.com>
2021-01-15 12:00:12 +0100

    Update UserDAO.java

--- src/es/unileon/iso/gpb/modelo/DAO/UserDAO.java
+++ src/es/unileon/iso/gpb/modelo/DAO/UserDAO.java
@@ -198,8 +198,26 @@ public class UserDAO extends DBConnection {
 
     }
 
-    public boolean deleteEmail() {
-        return false;
+    public boolean deleteEmail(String userID) {
+
+        try {
+            this.abrirConexion();
+
+            PreparedStatement stat = this.getConnection().prepareStatement("DELETE FROM user WHERE UserID=?");
+
+            stat.setString(1, userID);
+
+            stat.executeUpdate();
+
+            this.close();
+
+            return true;
+
+        } catch (Exception e) {
+            System.out.println(e);
+            return false;
+            //Llamar a controlador para sacar mensaje por vista TODO
+        }
 
     }
 
